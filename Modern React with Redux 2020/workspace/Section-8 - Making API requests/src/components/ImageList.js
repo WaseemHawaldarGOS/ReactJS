8d660ec8d6f976332717f276bc6beeab277639b8
WaseemHawaldarGOS <waseemhawaldar@gmail.com>
2020-07-07 10:33:36 +0530

    Added Section-9 - Building list of records code changes.

--- Modern React with Redux 2020/workspace/Section-8 - Making API requests/src/components/ImageList.js
+++ Modern React with Redux 2020/workspace/Section-8 - Making API requests/src/components/ImageList.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/*
+    key property was added to improve performance and also to remove warning shown in
+    key-addition-warning.png in section 9 images,
+    alt property was added to remove warning shown in alt-warning.png in section 9 images.
+*/
 const ImageList = props => {
   const images = props.images.map(({description, id, urls}) => {
     return <img alt = {description} key={id} src={urls.regular} />;
