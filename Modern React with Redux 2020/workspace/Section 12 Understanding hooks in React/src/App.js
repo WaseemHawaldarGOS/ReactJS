3a1b6fa5b55968162ca32d7f823f8cc20b919333
WaseemHawaldarGOS <waseemhawaldar@gmail.com>
2020-07-12 10:22:28 +0530

    Added Section-12 - Dropdown component

--- Modern React with Redux 2020/workspace/Section 12 Understanding hooks in React/src/App.js
+++ Modern React with Redux 2020/workspace/Section 12 Understanding hooks in React/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line
 import Accordion from './components/Accordion';
 // eslint-disable-next-line
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
 
 // eslint-disable-next-line
 const items = [
@@ -20,15 +22,41 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
 
 /*
     Enable <Accordion items={items}/> to see Accordian application and <Search/> to see Search application
 */
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Accordion items={items}/>
-      <Search/>
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
