4c830c2951402455be8f9334dd01a41f671ac1af
Waseem Hawaldar <waseemhawaldar@gmail.com>
2023-06-12 10:46:16 +0530

    updated

--- Modern React with Redux 2020/workspace/Section13 - Navigation from scratch/src/components/Route.js
+++ Modern React with Redux 2020/workspace/Section13 - Navigation from scratch/src/components/Route.js
@@ -8,6 +8,9 @@ const Route = ({ path, children }) => {
       setCurrentPath(window.location.pathname);
     };
 
+    /*The popstate event of the Window interface is fired when the active history entry changes while the user navigates the session history.
+        It changes the current history entry to that of the last page the user visited or,
+    if history.pushState() has been used to add a history entry to the history stack, that history entry is used instead.*/
     window.addEventListener('popstate', onLocationChange);
 
     return () => {
